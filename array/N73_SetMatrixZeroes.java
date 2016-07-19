4e8daa7ebc0ac85b884a0bcb2b5a19f1684ff664
Suixue Wang <wangsuixue@126.com>
2016-07-20 00:12:11 +0800

    modify first time

--- array/N73_SetMatrixZeroes.java
+++ array/N73_SetMatrixZeroes.java
@@ -1,29 +1,89 @@
 public class Solution {
     public void setZeroes(int[][] matrix) {
+    
+        // ------------------------------ 算法的空间复杂度O(1) ---------------------------------------
         int m = matrix.length;
         int n = matrix[0].length;
         
-        if(m==0 || n==0)
-            return ;
         
-        int [] flagRow = new int[m];
-        int [] flagColumn = new int[n];
+        boolean row_has_zeros = false; //第一行是否有0
+        boolean col_has_zeros = false; //第一列是否有0
         
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                if(matrix[i][j]==0){
-                    flagRow[i] = 1;
-                    flagColumn[j] = 1;
+        //先检查第一行和第一列是否有0,保存在O(1)的空间中
+        for(int j = 0; j < n; j++){
+            
+            if(matrix[0][j] == 0){
+                row_has_zeros = true;
+            }
+            
+        }  
+        
+        for(int i = 0; i < m; i++){
+            
+            if(matrix[i][0] == 0){
+                col_has_zeros = true;
+            }
+            
+        }  
+        
+        //利用第一行和第一列的空间存储其他位置是否出现0
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j < n; j++){
+                if(matrix[i][j] == 0){
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
             }
         }
         
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                if(flagRow[i]==1 || flagColumn[j]==1){
+        //将其他相应位置置为0
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j < n; j++){
+                if(matrix[i][0] == 0 || matrix[0][j] == 0){
                     matrix[i][j] = 0;
                 }
             }
         }
+
+        //如果第一行或者第一列出现0,则将整行或者整列置为0
+        if(row_has_zeros == true){
+        
+            for(int j = 0; j < n; j++){
+                matrix[0][j] = 0;
+            }
+        }
+        
+        if(col_has_zeros == true){
+            
+            for(int i = 0; i < m; i++){
+                matrix[i][0] = 0;
+            }
+        }
+        
+
+        // ------------------------------ 算法的空间复杂度O(m+n) ---------------------------------------
+        // int m = matrix.length;
+        // int n = matrix[0].length;
+        
+        // int[] row = new int[m];
+        // int[] col = new int[n];
+        
+        // for(int i = 0; i < m; i++){
+        //     for(int j = 0; j < n; j++){
+        //         if(matrix[i][j] == 0){
+        //             row[i] = 1;
+        //             col[j] = 1;
+        //         }
+        //     }
+        // }
+        
+        // for(int i = 0; i < m; i++){
+        //     for(int j = 0; j < n; j++){
+        //         if(row[i] == 1 || col[j] == 1){
+        //             matrix[i][j] = 0;
+        //         }
+        //     }
+        // }
+        
     }
 }
