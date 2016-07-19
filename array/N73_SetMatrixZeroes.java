30c29e952a9e69fad95e7df2fb0932b55274377d
Suixue Wang <wangsuixue@126.com>
2016-07-19 23:43:19 +0800

    add 1 file

---
+++ array/N73_SetMatrixZeroes.java
@@ -0,0 +1,29 @@
+public class Solution {
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        
+        if(m==0 || n==0)
+            return ;
+        
+        int [] flagRow = new int[m];
+        int [] flagColumn = new int[n];
+        
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(matrix[i][j]==0){
+                    flagRow[i] = 1;
+                    flagColumn[j] = 1;
+                }
+            }
+        }
+        
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(flagRow[i]==1 || flagColumn[j]==1){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+}
