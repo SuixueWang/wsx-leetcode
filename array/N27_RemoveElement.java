dbc519f35c6aab18efcafb1f0a05f304c4b9af37
Suixue Wang <wangsuixue@126.com>
2016-07-22 14:48:17 +0800

    modify 1 file

--- array/N27_RemoveElement.java
+++ array/N27_RemoveElement.java
@@ -1,14 +1,28 @@
 public class Solution {
     public int removeElement(int[] nums, int val) {
 
-        int index = 0;
-        for(int i = 0; i < nums.length; i++){
-            if(nums[i] != val){
-                nums[index++] = nums[i];
+        //双指针
+        int i = 0;
+        int j = 0;
+        while(j < nums.length){
+            if(nums[j] == val){
+                j++;
+            }else{
+                nums[i++] = nums[j++];
             }
         }
         
-        return index;
+        return i;
+
+
+        // int index = 0;
+        // for(int i = 0; i < nums.length; i++){
+        //     if(nums[i] != val){
+        //         nums[index++] = nums[i];
+        //     }
+        // }
+        
+        // return index;
 
     }
 }
