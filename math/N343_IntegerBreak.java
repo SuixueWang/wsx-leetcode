4efd676d4bd60a5699aa0824820edcadada27ed3
Suixue Wang <wangsuixue@126.com>
2016-07-28 19:57:39 +0800

    1 file

---
+++ math/N343_IntegerBreak.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public int integerBreak(int n) {
+        
+        if(n == 2 || n == 3) return n-1;
+        
+        int result = 1;
+        
+        while(n > 4){
+            result = result * 3;
+            n = n - 3;
+        }
+        
+        return result * n;
+    }
+}
